ddfb2bad32ba643ee0a9dfa9b3e2caa6346d5e71
Jon Petto <jonpetto@gmail.com>
2012-12-20 14:10:49 -0600

    optimized js/css, cleaned up markup

--- media/js/firefox/new.js
+++ media/js/firefox/new.js
@@ -7,23 +7,12 @@
 
     // Load images on load so as not to block the loading of other images.
     $(window).on('load', function() {
-        // Replace install images depending on the user's platform.
-        var $install1 = $('#install1');
-        var $install2 = $('#install2');
-        var $install3 = $('#install3');
-        var img_os = (site.platform === 'osx') ? 'mac' : 'win';
-
-        $('html').addClass('ready-for-scene2');
-
-        $install2.addClass(img_os);
-        $install3.addClass(img_os);
-
         // Screen 1 is unique for IE < 9
         if (site.platform === 'windows' && $.browser.msie && $.browser.version < 9) {
-            img_os = 'winIE8';
+            $('html').addClass('winIE8');
         }
 
-        $install1.addClass(img_os);
+        $('html').addClass('ready-for-scene2');
     });
 
     // Bind events on domReady.
